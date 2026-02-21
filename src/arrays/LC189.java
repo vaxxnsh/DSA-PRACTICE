4adad83a5f88747d601eabcf986e3eac87da654b
vaxxnsh <kakshit817@gmail.com>
2026-02-21 07:39:03 +0530

    question: LC189

---
+++ src/arrays/LC189.java
@@ -0,0 +1,20 @@
+package arrays;
+
+public class LC189 {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        reverse(0,n-k-1,nums);
+        reverse(n-k,n-1,nums);
+        reverse(0,n-1,nums);
+    }
+
+    public void reverse(int i,int j,int[] arr) {
+        while(i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
