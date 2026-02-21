088c7173599f5af41af1fb03cc8fac9bc241d9d3
vaxxnsh <kakshit817@gmail.com>
2026-02-21 07:44:49 +0530

    fix: LC189, added missing edge case

--- src/arrays/LC189.java
+++ src/arrays/LC189.java
@@ -3,6 +3,8 @@ package arrays;
 public class LC189 {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
+        k = k % n;
+        if(k == 0) return;
         reverse(0,n-k-1,nums);
         reverse(n-k,n-1,nums);
         reverse(0,n-1,nums);
