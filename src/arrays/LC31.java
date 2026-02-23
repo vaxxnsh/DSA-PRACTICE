cb03d45f91fcc96201665c805a8f5b73c5565f7f
vaxxnsh <kakshit817@gmail.com>
2026-02-23 23:49:18 +0530

    question: LC31

---
+++ src/arrays/LC31.java
@@ -0,0 +1,42 @@
+package arrays;
+
+public class LC31 {
+    public void nextPermutation(int[] nums) {
+        int index = -1, n=nums.length;
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index == -1) {
+            reverse(nums, 0, nums.length - 1);
+            return;
+        }
+
+        for (int i = nums.length - 1; i > index; i--) {
+            if (nums[i] > nums[index]) {
+                swap(nums, i, index);
+                break;
+            }
+        }
+
+        reverse(nums, index + 1, nums.length - 1);
+    }
+
+    void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
+    }
+
+    void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
